9f2805df37907e5e3754a52a1802715950b7fe74
DFRodriguezL <dfrodriguezl@dane.gov.co>
2021-04-26 12:10:51 -0500

    Adición custom drawer

--- lib/src/views/ui/InitialMap.dart
+++ lib/src/views/ui/InitialMap.dart
@@ -45,8 +45,9 @@ class InitialMap extends StatelessWidget {
   Widget build(BuildContext context) {
     final MapController mapController = MapController();
     final List<Marker> userLocationMarkers = <Marker>[];
+    GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
     // List<Marker> centroidsManzanas
-    final PopupController _popupLayerController = PopupController();
+    // final PopupController _popupLayerController = PopupController();
     // final PopupSnap popupSnap;
     //
     // MapPageScaffold(this.popupSnap);
@@ -57,31 +58,31 @@ class InitialMap extends StatelessWidget {
     main();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Buscar ubicación...",
-        style: TextStyle(
-        color: Color(0xffffffff),
-      ),),
-      backgroundColor: Colors.transparent,
-      actions: [
-        IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){
-              final snackBar = SnackBar(
-                content: Text('Buscar!!!'),
-                action: SnackBarAction(
-                  label: 'Atrás',
-                  onPressed: () {
-                    // Some code to undo the change.
-                  },
-                ),
-              );
-
-              // Find the ScaffoldMessenger in the widget tree
-              // and use it to show a SnackBar.
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            })
-      ],),
+      // appBar: AppBar(
+      //   title: Text("Buscar ubicación...",
+      //   style: TextStyle(
+      //   color: Color(0xffffffff),
+      // ),),
+      // backgroundColor: Colors.transparent,
+      // actions: [
+      //   IconButton(
+      //       icon: Icon(Icons.search),
+      //       onPressed: (){
+      //         final snackBar = SnackBar(
+      //           content: Text('Buscar!!!'),
+      //           action: SnackBarAction(
+      //             label: 'Atrás',
+      //             onPressed: () {
+      //               // Some code to undo the change.
+      //             },
+      //           ),
+      //         );
+      //
+      //         // Find the ScaffoldMessenger in the widget tree
+      //         // and use it to show a SnackBar.
+      //         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      //       })
+      // ],),
       drawer: Drawer(
         child: ListView(
           // Important: Remove any padding from the ListView.
@@ -112,123 +113,170 @@ class InitialMap extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(0),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 0, bottom: 0),
-              // child: locationText()
-              // child: Text('This is a map that is showing (-74, 4).'),
-            ),
-            Flexible(
-              child: FlutterMap(
-                mapController: mapController,
-                options: MapOptions(
-                  plugins: <MapPlugin>[
-                    LocationPlugin()
-                  ],
-                  interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
-                  center: LatLng(4, -74),
-                  zoom: 13.0,
-                ),
-                layers: [
-                  TileLayerOptions(
-                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                      subdomains: ['a', 'b', 'c']
-                  ),
-                  MarkerLayerOptions(markers: userLocationMarkers),
-                  // MarkerLayerOptions(
-                  //   markers: [
-                  //     Marker(
-                  //       width: 80.0,
-                  //       height: 80.0,
-                  //       point: LatLng(4, -74),
-                  //       builder: (ctx) =>
-                  //           Container(
-                  //             child: FlutterLogo(),
-                  //           ),
-                  //     ),
-                  //   ],
-                  // ),
-                  LocationOptions(
-                    markers: userLocationMarkers,
-                    onLocationUpdate: (LatLngData ld) {
-                      print('Location updated: ${ld?.location}');
-                    },
-                    onLocationRequested: (LatLngData ld) {
-                      if (ld == null || ld.location == null) {
-                        return;
-                      }
-                      mapController?.move(ld.location, 18.0);
-                    },
-                    buttonBuilder: (BuildContext context,
-                        ValueNotifier<LocationServiceStatus> status,
-                        Function onPressed) {
-                      return Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 16.0, right: 16.0),
-                          child: Column(
-                              children: [
-                                SizedBox(height: 10),
-                                FloatingActionButton(
-                                  heroTag: "btn1",
-                                  backgroundColor: hexToColor("#B91450"),
-                                  child: ValueListenableBuilder<LocationServiceStatus>(
-                                      valueListenable: status,
-                                      builder: (BuildContext context,
-                                          LocationServiceStatus value, Widget child) {
-                                        switch (value) {
-                                          case LocationServiceStatus.disabled:
-                                          case LocationServiceStatus.permissionDenied:
-                                          case LocationServiceStatus.unsubscribed:
-                                            return const Icon(
-                                              Icons.location_disabled,
-                                              color: Color(0xffffffff),
-                                              // color: Colors.white,
-                                            );
-                                            break;
-                                          default:
-                                            return const Icon(
-                                              Icons.gps_fixed,
-                                              color: Color(0xffffffff),
-                                            );
-                                            break;
-                                        }
-                                      }),
-                                  onPressed: () => onPressed()),
-                                SizedBox(height: 10),
-                                FloatingActionButton(
-                                  heroTag: "btn2",
-                                  backgroundColor: hexToColor("#B91450"),
-                                  onPressed: () {
-                                    Navigator.push(context, 
-                                    MaterialPageRoute(
-                                        builder: (_) => ARCoreScreen()));
-                                  },
-                                  child: Icon(Icons.add_location_alt_outlined,
-                                    color: Color(0xffffffff),),
-                                  )]),
-                        ),
-                      );
-                    },
-                  ),
-                MarkerLayerOptions(
-                  markers: centroidsManzanas,
-                ),
-                  // PopupMarkerLayerOptions(
-                  //   markers: centroidsManzanas,
-                  //   // popupSnap: widget.popupSnap,
-                  //   popupController: _popupLayerController,
-                  //   popupBuilder: (BuildContext _, Marker marker) => ExamplePopup(marker),
-                  // ),
-                ],
+      body: Stack(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(0),
+              child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 0, bottom: 0),
+                      // child: locationText()
+                      // child: Text('This is a map that is showing (-74, 4).'),
+                    ),
+                    Flexible(
+                        child: FlutterMap(
+                          mapController: mapController,
+                          options: MapOptions(
+                            plugins: <MapPlugin>[
+                              LocationPlugin()
+                            ],
+                            interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+                            center: LatLng(4, -74),
+                            zoom: 13.0,
+                          ),
+                          layers: [
+                            TileLayerOptions(
+                                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                subdomains: ['a', 'b', 'c']
+                            ),
+                            MarkerLayerOptions(markers: userLocationMarkers),
+                            // MarkerLayerOptions(
+                            //   markers: [
+                            //     Marker(
+                            //       width: 80.0,
+                            //       height: 80.0,
+                            //       point: LatLng(4, -74),
+                            //       builder: (ctx) =>
+                            //           Container(
+                            //             child: FlutterLogo(),
+                            //           ),
+                            //     ),
+                            //   ],
+                            // ),
+                            LocationOptions(
+                              markers: userLocationMarkers,
+                              onLocationUpdate: (LatLngData ld) {
+                                print('Location updated: ${ld?.location}');
+                              },
+                              onLocationRequested: (LatLngData ld) {
+                                if (ld == null || ld.location == null) {
+                                  return;
+                                }
+                                mapController?.move(ld.location, 18.0);
+                              },
+                              buttonBuilder: (BuildContext context,
+                                  ValueNotifier<LocationServiceStatus> status,
+                                  Function onPressed) {
+                                return Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 100.0, right: 16.0),
+                                    child: Column(
+                                        children: [
+                                          SizedBox(height: 10),
+                                          FloatingActionButton(
+                                              heroTag: "btn1",
+                                              backgroundColor: hexToColor("#B91450"),
+                                              child: ValueListenableBuilder<LocationServiceStatus>(
+                                                  valueListenable: status,
+                                                  builder: (BuildContext context,
+                                                      LocationServiceStatus value, Widget child) {
+                                                    switch (value) {
+                                                      case LocationServiceStatus.disabled:
+                                                      case LocationServiceStatus.permissionDenied:
+                                                      case LocationServiceStatus.unsubscribed:
+                                                        return const Icon(
+                                                          Icons.location_disabled,
+                                                          color: Color(0xffffffff),
+                                                          // color: Colors.white,
+                                                        );
+                                                        break;
+                                                      default:
+                                                        return const Icon(
+                                                          Icons.gps_fixed,
+                                                          color: Color(0xffffffff),
+                                                        );
+                                                        break;
+                                                    }
+                                                  }),
+                                              onPressed: () => onPressed()),
+                                          SizedBox(height: 10),
+                                          FloatingActionButton(
+                                            heroTag: "btn2",
+                                            backgroundColor: hexToColor("#B91450"),
+                                            onPressed: () {
+                                              Navigator.push(context,
+                                                  MaterialPageRoute(
+                                                      builder: (_) => ARCoreScreen()));
+                                            },
+                                            child: Icon(Icons.add_location_alt_outlined,
+                                              color: Color(0xffffffff),),
+                                          ),
+                                          // FloatingActionButton(
+                                          //   heroTag: "btn3",
+                                          //   backgroundColor: hexToColor("#B91450"),
+                                          //   onPressed: () {
+                                          //     // Navigator.push(context,
+                                          //     //     MaterialPageRoute(
+                                          //     //         builder: (_) => ARCoreScreen()));
+                                          //   },
+                                          //   child: Icon(Icons.menu,
+                                          //     color: Color(0xffffffff),),
+                                          // )
+                                        ]),
+
+
+                                  ),
+                                );
+                              },
+                            ),
+                            MarkerLayerOptions(
+                              markers: centroidsManzanas,
+                            ),
+                            // PopupMarkerLayerOptions(
+                            //   markers: centroidsManzanas,
+                            //   // popupSnap: widget.popupSnap,
+                            //   popupController: _popupLayerController,
+                            //   popupBuilder: (BuildContext _, Marker marker) => ExamplePopup(marker),
+                            // ),
+                          ],
+                        )
+                    )
+                  ]
               )
-            )
-          ]
-        )
+          ),
+         Padding(
+           padding: const EdgeInsets.only(top: 16.0, left: 10.0),
+             child: Column(
+                 children: [
+                   SizedBox(height: 10),
+                   Builder(
+                     builder: (context) =>
+                         FloatingActionButton(
+                           heroTag: "btn3",
+                           backgroundColor: hexToColor("#B91450"),
+                           onPressed: () {
+                             // _scaffoldKey.currentState.openDrawer();
+                             Scaffold.of(context).openDrawer();
+                             // Navigator.push(context,
+                             //     MaterialPageRoute(
+                             //         builder: (_) => ARCoreScreen()));
+                           },
+                           child: Icon(Icons.menu,
+                             color: Color(0xffffffff),),
+                         )
+                   )
+
+                 ]
+             )
+         )
+
+
+        ]
       )
+
+
     );
 
 
